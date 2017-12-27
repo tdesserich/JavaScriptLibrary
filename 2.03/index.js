316b850bbaaf68988a97bc9762b880312fd48c84
Tracy Desserich <tdesserich@gmail.com>
2017-12-27 18:51:14 -0500

    Added module 2.03

---
+++ 2.03/index.js
@@ -0,0 +1,5 @@
+// This code works the modern way - can also be in single quotes. 
+// Make sure that "use strict" is on the top of the script, or the strict mode may be enabled only for a certain function.
+// It’s always recommended to start scripts with "use strict"
+
+"use strict";
