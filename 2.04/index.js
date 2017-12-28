d489959b8979b39d3d5445ad2ffa01a7e386a49b
Tracy Desserich <tdesserich@gmail.com>
2017-12-27 19:24:29 -0500

    Added module 2.04

---
+++ 2.04/index.js
@@ -0,0 +1,17 @@
+'strict';
+
+let admin , name;
+
+name='John';
+
+admin=name;
+
+alert( admin );
+
+let ourPlanetName='Earth';
+
+let currentUserName;
+
+const BIRTHDAY = '18.04.1982';
+
+const age = someCode(BIRTHDAY);
